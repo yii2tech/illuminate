fd2c521327b2e4d9b794dfdb3a976a6c45d8a9ad
Paul Klimov <klimov.paul@gmail.com>
2019-03-07 19:49:26 +0200

    unit test for password hashing added

---
+++ tests/Yii/Security/PasswordHashTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Yii2tech\Illuminate\Test\Yii\Security;
+
+use yii\base\Security;
+use Illuminate\Hashing\BcryptHasher;
+use Yii2tech\Illuminate\Test\TestCase;
+
+class PasswordHashTest extends TestCase
+{
+    public function testPasswordHash()
+    {
+        $yiiSecurity = new Security(['passwordHashCost' => 4]);
+        $illuminateHasher = new BcryptHasher(['rounds' => 4]);
+
+        $password = 'secret';
+
+        $yiiHash = $yiiSecurity->generatePasswordHash($password);
+
+        $illuminateHash = $illuminateHasher->make($password);
+
+        $this->assertTrue($illuminateHasher->check($password, $yiiHash));
+        $this->assertTrue($yiiSecurity->validatePassword($password, $illuminateHash));
+    }
+}
